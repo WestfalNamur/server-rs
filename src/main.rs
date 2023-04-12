ce892c316b050921f9c5d803840b49d8b7671611
Stefan Crummenerl <crummenerls@gmail.com>
2023-04-12 21:24:36 +0200

    example: router composition

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::net::SocketAddr;
 
 use axum;
-use axum::extract::Query;
 use axum::extract::Path;
+use axum::extract::Query;
 use axum::response::Html;
 use axum::response::IntoResponse;
 use axum::routing::get;
@@ -11,24 +11,27 @@ use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new()
-        .route("/hello", get(handler_hello))
-        .route("/hello2/:name", get(handler_hello2));
+    let routes_all = Router::new().merge(routes_hello());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("Listening on: {addr}\n");
 
     axum::Server::bind(&addr)
-        .serve(routes_hello.into_make_service())
+        .serve(routes_all.into_make_service())
         .await
         .unwrap();
 }
 
 // region: Handler
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
+}
 
 #[derive(Debug, Deserialize)]
-struct HelloParams{
-    name: Option<String>
+struct HelloParams {
+    name: Option<String>,
 }
 
 // Query extractor allows us to get parameter from query i.e. /hello?name=Jane
